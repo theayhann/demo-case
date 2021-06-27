8500b01186721c5059ba5c17824146abad817e0a
Emre Solmaz <emrslmz57@gmail.com>
2021-06-27 22:37:07 +0300

    added

---
+++ src/components/edit.vue
@@ -0,0 +1,104 @@
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel carousel-multi-item" data-ride="carousel">
+
+<!--Slides-->
+<div class="carousel-inner" role="listbox">
+
+  <!--First slide-->
+  <div class="carousel-item active">
+
+    <div class="row">
+      <div class="">
+        <div class="item">
+          <div>
+            <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+          </div>
+          <div class="item-text">
+            <b>Brave New World</b>
+            <small class="item-date">2018 . 1h 23m</small>
+          </div>
+          <div class="video-mouseover-bottom">
+            <div>
+              <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+              <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+              <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class=" clearfix d-none d-md-block">
+        <div class="item">
+          <div>
+            <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+          </div>
+          <div class="item-text">
+            <b>Vanguard</b>
+            <small class="item-date">2018 . 1h 23m</small>
+          </div>
+          <div class="video-mouseover-bottom">
+            <div>
+              <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+              <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+              <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class=" clearfix d-none d-md-block">
+        <div class="item">
+          <div>
+            <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+          </div>
+          <div class="item-text">
+            <b>Bad Times At The El Royale</b>
+            <small class="item-date">2018 . 1h 23m</small>
+          </div>
+          <div class="video-mouseover-bottom">
+            <div>
+              <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+              <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+              <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/.First slide-->
+
+  <!--Second slide-->
+  <div class="carousel-item">
+
+    <div class="row">
+      <div class="">
+        <div class="item">
+          <div>
+            <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+          </div>
+          <div class="item-text">
+            <b>John Wick 3 Parabellum</b>
+            <small class="item-date">2018 . 1h 23m</small>
+          </div>
+          <div class="video-mouseover-bottom">
+            <div>
+              <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+              <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+              <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+  <!--/.Second slide-->
+
+</div>
+<!--/.Slides-->
+
+</div>
+<!--/.Carousel Wrapper-->
---
+++ src/components/new.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class=" d-flex justify-content-between align-items-center my-4">
+
+
+    <div id="multi-item-example" class="carousel carousel-multi-item" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active d-flex justify-content-center">
+          <div class="row">
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="carousel-item">
+          <div class="row">
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World111</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+            <div class="item clearfix d-none d-md-block">
+              <div>
+                <img class="item-picture" src="/assets/images/videos-pic.png" alt="videos">
+              </div>
+              <div class="item-text">
+                <b>Brave New World</b>
+                <small class="item-date">2018 . 1h 23m</small>
+              </div>
+              <div class="video-mouseover-bottom">
+                <div>
+                  <button class="video-mouseover-buttons"><i class="far fa-heart"></i></button>
+                  <button class="video-mouseover-buttons video-mouseover-button-play"><i class="fab fa-google-play"></i></button>
+                  <button class="video-mouseover-buttons"><i class="fas fa-heart-broken"></i></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+    <!--Controls-->
+    <div class="item-controls">
+      <a href="#multi-item-example" data-slide="next"><i class="fa item-controls__icon fa-chevron-right"></i></a>
+    </div>
+    <!--/.Controls-->
+
+
+  </div>
+</template>
